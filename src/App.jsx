e098a68ffca85cd4a97012c651f71c1ead7a1b18
toqa-saad97 <toqasaad1997@gmail.com>
2024-11-06 22:56:51 +0200

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import "./index.css";
+import Layout from "./Component/Layout";
+import Cabin from "./pages/Cabin";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalStyles from "./styles/GlobalStyles";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "cabin", element: <Cabin /> },
+    ],
+  },
+]);
 
 function App() {
-
-
   return (
-<>
-</>)
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyles/>
+      <RouterProvider router={router} />
+      <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
